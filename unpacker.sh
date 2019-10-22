b31a22ea09dfc3d293a8b707d93a1a1eb4fd78d4
Tom Gustafsson <enigustom@eniram.fi>
2019-10-22 16:14:32 +0300

    Add option to extract config file

--- unpacker.sh
+++ unpacker.sh
@@ -2,8 +2,8 @@
 tar_file=${1:-None}
 if [ "$tar_file" = "None" ]
 then
-    echo "Extract layers from a saved Docker image into a directory."
-    echo "Usage: ./unpacker.sh IMAGE DIRECTORY"
+    echo "Extract layers and, optionally, config from a saved Docker image."
+    echo "Usage: ./unpacker.sh IMAGE DIRECTORY [CONFIG]"
     exit 1
 fi
 layers_cache_dir=${2:-None}
@@ -12,10 +12,19 @@ then
     echo "Missing layer cache directory! See ./unpacker.sh for instructions."
     exit 1
 fi
+config=${3:-None}
+
 tmp_dir=$(mktemp -d -t image-XXXXXXXXXX)
 
 echo "Unpacking '$tar_file' to '$tmp_dir' ..."
 tar -C $tmp_dir -xf $tar_file
+
+if [ "$config" = "None" ]
+then
+    echo "Skipping image config file ..."
+else
+    mv $tmp_dir/`ls $tmp_dir | grep json | grep -v manifest` $config
+fi
 
 echo "Moving layers to '$layers_cache_dir' ..."
 find $tmp_dir \
